eec9c372f2003e57b4e21714aa69fe4cd6eb9bed
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-01-07 22:18:11 +0200

    create data.js

--- commands/cable.js
+++ commands/cable.js
@@ -1,7 +1,7 @@
 module.exports = {
-	run: (client, message, args) => {
+	run: async (client, message, args) => {
 		message.delete();
-		message.channel.send({ files: ["https://cdn.discordapp.com/attachments/787590275110273035/789403313329405952/A511drhyO4AAAAASUVORK5CYII.png"] });
+		await message.channel.send({ files: ["https://cdn.discordapp.com/attachments/787590275110273035/789403313329405952/A511drhyO4AAAAASUVORK5CYII.png"] });
 		message.channel.send({
 			embed: {
 				"title": "__Bandwidth Calculator__",
