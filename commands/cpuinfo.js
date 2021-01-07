f930ed3c898c3ee315bd7b109533c127728cdec8
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-01-07 22:38:18 +0200

    create cpuinfo.js

---
+++ commands/cpuinfo.js
@@ -0,0 +1,11 @@
+module.exports = {
+	run: (client, message, args) => {
+		message.channel.send({
+			embed: {
+				"title": "Command Infomation",
+				"description": "This command does not search the web, TÆMBØ, Finn, and High Quality TNT have to manually add each CPU and search it's specs (why? cuz TÆ still learning how to code a bot) so don't expect your CPU to be on here. To use the command, type ``,cpuintel`` or ``,cpuamd``, space, then the cpu model, do not include the family the cpu is in like Ryzen 9 or i5, just the model. like ``,cpuintel 10400``."
+			}
+		})
+	},
+	name: 'cpuinfo'
+};
--- index.js
+++ index.js
@@ -41,12 +41,6 @@ client.on("message", (message) => {
 			message.channel.send('An error occured while executing that command.');
 		}
 	}
-	if (message.content.startsWith(prefix + "cpuinfo")) {
-		message.channel.send({embed: {
-			"title": "Command Infomation",
-			"description": "This command does not search the web, TÆMBØ, Finn, and High Quality TNT have to manually add each CPU and search it's specs (why? cuz TÆ still learning how to code a bot) so don't expect your CPU to be on here. To use the command, type ``,cpuintel`` or ``,cpuamd``, space, then the cpu model, do not include the family the cpu is in like Ryzen 9 or i5, just the model. like ``,cpuintel 10400``."
-		}})
-	}
 	if (message.content.startsWith(prefix + "gpuinfo")) {
 		message.channel.send({embed: {
 			"title": "Command Infomation (__Very Very Early Beta Alpha New Unfinished In Progress Thingy Yes__)",
