f8151cf51fa079a1502e7e8ff363ff399fb7c3bd
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-01-20 19:53:18 +0200

    Create emulator.js

---
+++ commands/emulator.js
@@ -0,0 +1,9 @@
+module.exports = {
+	run: (client, message, args) => {
+		message.channel.send('To play PC Creator on PC, you need to use an emulator. The game is not available on Steam or any other PC game launcher. The most popular emulators are Bluestacks, MEmu, NoxPlayer');
+	},
+	name: 'emulator',
+	alias: ['bluestacks', 'memu'],
+	category: 'PC Creator',
+	description: 'Info about how to play PC Creator on PC'
+};
