b11cc823ce12435be1f0dc63cb13c0a53bbb1b76
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2021-01-22 17:50:05 -0800

    updated staff, added 4th gen intel, intel core 2

--- commands/cpuinfo.js
+++ commands/cpuinfo.js
@@ -3,7 +3,7 @@ module.exports = {
 		message.channel.send({
 			embed: {
 				"title": "Command Infomation",
-				"description": "This command does not search the web, TÆMBØ, Finn, and High Quality TNT have to manually add each CPU and search it's specs (why? cuz TÆ still learning how to code a bot) so don't expect your CPU to be on here. To use the command, type ``,cpuintel`` or ``,cpuamd``, space, then the cpu model, do not include the family the cpu is in like Ryzen 9 or i5, just the model. like ``,cpuintel 10400``."
+				"description": "This command does not search the web, TÆMBØ, Finn, Apollo, and High Quality TNT have to manually add each CPU and search it's specs (why? cuz TÆ still learning how to code a bot) so don't expect your CPU to be on here. To use the command, type ``,cpuintel`` or ``,cpuamd``, space, then the cpu model, do not include the family the cpu is in like Ryzen 9 or i5, just the model. like ``,cpuintel 10400``."
 			}
 		})
 	},
--- commands/staff.js
+++ commands/staff.js
@@ -2,7 +2,7 @@ module.exports = {
     run: (client, message, args) => {
         message.channel.send({embed: {
 			"title": "__Staff members__",
-			"description": "\n**<@&632674518317531137>**\n<@387088097374109698>\n\n**<@&589435378147262464>**\n<@506033590656696332>\n<@615761944154210305>\n<@478922521601638400>\n\nIf you need help, or want to report bad behavior, feel free to message anyone of these people and they will get back to you asap.",
+			"description": "**<@&632674518317531137>**\n<@387088097374109698>\n\n**<@&589435378147262464>**\n<@506033590656696332>\n<@615761944154210305>\n<@478922521601638400>\n\nIf you want to report someone or need any other moderation help, feel free to message anyone of these people.\n\n**<@&697728131003580537>**\n<@731186471461060641>\n<@438760488965242883>\n<@776552252609396736>\nIf you have a question with the game, you are open to ping or message a helper to receive help.",
 			"color": 3971825}});
     },
 	name: 'staff',
