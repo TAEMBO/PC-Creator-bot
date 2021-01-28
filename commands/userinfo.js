e7ff7fbaf303fc209da8226f6afd9751701649a4
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2021-01-27 17:38:17 -0800

    updated userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,12 +1,12 @@
 module.exports = {
 	run: async (client, message, args) => {
 		const member = message.mentions.members?.first() || (await message.guild.members.fetch(args[1]).catch(() => undefined));
-		if (!member) return message.channel.send('You failed to mention a member.');
+		if (!member) return message.channel.send('You failed to mention a user from this server.');
 		const embed = new client.embed()
 			.setTitle(`Userinfo: ${member.user.tag}`)
 			.addField(':small_blue_diamond: Account Creation Date', `${member.user.createdAt.getUTCFullYear()}-${('0' + (member.user.createdAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.user.createdAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
 			.addField(':small_blue_diamond: Join Date', `${member.joinedAt.getUTCFullYear()}-${('0' + (member.joinedAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.joinedAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
-			.addField(':small_blue_diamond: ID, Nickname and Tag', `ID: ${member.user.id}\nNickname: ${member.nickname ? member.nickname : 'None'}\nTag: ${member.user.tag}`)
+			.addField(':small_blue_diamond: ID, Nickname, and Tag', `ID: ${member.user.id}\nNickname: ${member.nickname ? member.nickname : 'None'}\nTag: ${member.toString()}`)
 			.setColor(3971825)
 			.setThumbnail(member.user.avatarURL({ format: 'png', dynamic: true, size: 2048}))
 		message.channel.send(embed);
