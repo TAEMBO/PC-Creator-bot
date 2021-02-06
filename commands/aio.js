e4d7d3ff88d2d426ab8e4dafc0a4394a01080a42
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2021-02-05 21:31:50 -0800

    fuqib aio fix

--- commands/aio.js
+++ commands/aio.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: (client, message, args) => {
-		message.channel.send({ files: ["https://cdn.discordapp.com/attachments/696448442989543445/778674903749492736/IMG_0716_2020_11_08_17_40_41_UTC.PNG"] });
+		message.channel.send({ files: ["https://cdn.discordapp.com/attachments/571031705109135361/807483439695396934/IMG_0716_2020_11_08_17_40_41_UTC.PNG"] });
 	},
 	name: 'aio',
 	description: 'Provides a picture of what a watercooler looks like',
