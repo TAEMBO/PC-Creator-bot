10413b3b7c378b5b7c0ce346c121e0d5c194c826
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-02-23 23:05:54 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,14 +1,14 @@
 module.exports = {
 	run: async (client, message, args) => {
-		if (!args[1]) return message.channel.send('You failed to mention a user.');
-		const member = message.mentions.members?.first() || (await message.guild.members.fetch(args[1]).catch(() => undefined));
+		const member = args[1] ? message.mentions.members?.first() || (await message.guild.members.fetch(args[1]).catch(() => undefined)) : message.member;
 		if (!member) return message.channel.send('You failed to mention a user from this server.');
 		const embed = new client.embed()
 			.setTitle(`User info: ${member.user.tag}`)
 			.addField(':small_blue_diamond: Account Creation Date', `${member.user.createdAt.getUTCFullYear()}-${('0' + (member.user.createdAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.user.createdAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
 			.addField(':small_blue_diamond: Join Date', `${member.joinedAt.getUTCFullYear()}-${('0' + (member.joinedAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.joinedAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
 			.addField(':small_blue_diamond: ID, Nickname, and Mention', `ID: ${member.user.id}\nNickname: ${member.nickname ? member.nickname : 'None'}\nMention: ${member.toString()}`)
-			.setColor(member.displayColor || '#bbbbbb')
+			.addField(':small_blue_diamond: Roles', member.roles.cache.filter(x => x.id !== message.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x.name).join('\n').slice(0, 1024))
+			.setColor(member.displayColor || '#fefefe')
 			.setThumbnail(member.user.avatarURL({ format: 'png', dynamic: true, size: 2048}))
 		message.channel.send(embed);
 	},
