a7980889545c57c33491ea88f9f13cb762c3ef8d
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-02-25 20:33:08 +0200

    roles  = 0

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,7 +7,7 @@ module.exports = {
 			.addField(':small_blue_diamond: Account Creation Date', `${member.user.createdAt.getUTCFullYear()}-${('0' + (member.user.createdAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.user.createdAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
 			.addField(':small_blue_diamond: Join Date', `${member.joinedAt.getUTCFullYear()}-${('0' + (member.joinedAt.getUTCMonth() + 1)).slice(-2)}-${('0' + member.joinedAt.getUTCDate()).slice(-2)} (YYYY-MM-DD)`)
 			.addField(':small_blue_diamond: ID, Nickname, and Mention', `ID: ${member.user.id}\nNickname: ${member.nickname ? member.nickname : 'None'}\nMention: ${member.toString()}`)
-			.addField(':small_blue_diamond: Roles', member.roles.cache.filter(x => x.id !== message.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x.name).join('\n').slice(0, 1024))
+			.addField(':small_blue_diamond: Roles', member.roles.cache.size > 0 ? member.roles.cache.filter(x => x.id !== message.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x.name).join('\n').slice(0, 1024) : 'None')
 			.setColor(member.displayColor || '#fefefe')
 			.setThumbnail(member.user.avatarURL({ format: 'png', dynamic: true, size: 2048}))
 		message.channel.send(embed);
