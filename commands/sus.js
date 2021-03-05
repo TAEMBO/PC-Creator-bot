dd0e33907504c051dbeaa49c4aa71d05f97cb747
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-03-05 17:23:05 +0200

    sus 4% actualyl works now

--- commands/sus.js
+++ commands/sus.js
@@ -1,10 +1,9 @@
 module.exports = {
 	run: (client, message, args) => {
 		// 20% of the time responds with a ping
-		if (Math.random() < 0.2) message.channel.send(message.member.toString() + (Math.random < 0.2 ? ' is a fucking cunt' : ' is sus!'));
+		if (Math.random() < 0.2) message.channel.send(message.member.toString() + (Math.random() < 0.2 ? ' is a fucking cunt' : ' is sus!'));
 	},
 	name: 'sus',
 	description: 'GuildMember deserialization with binary tree inversion',
 	category: 'Moderation',
-	usage: ['go', 'fuck', 'yourself', 'chikkenn']
 };
