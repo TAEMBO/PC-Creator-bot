d75d309a03033c59e342809a46f9d73593418689
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-03-22 20:20:42 +0200

    make ,virus better

--- commands/virus.js
+++ commands/virus.js
@@ -1,6 +1,11 @@
 module.exports = {
     run: (client, message, args) => {
-        message.channel.send("Go to the online shop in the menu, and under the OS section you will find a list of programs. Select either Eset or Avast (they’re the same) and buy it. Then go to customer’s PC and turn it on to the desktop, next press the install button, followed by the small backpack icon, you will then see your antivirus there where you can download it.");
+		const embed = new client.embed()
+			.setTitle('Viruses in PC Creator')
+			.setDescription("Go to the online shop in the menu, and under the OS section you will find a list of programs. Select either Eset or Avast (they’re the same) and buy it. Then go to customer’s PC and turn it on to the desktop, next press the install button, followed by the small backpack icon, you will then see your antivirus there where you can download it.")
+			.setImage('https://media.discordapp.net/attachments/571031705109135361/823605061003378708/antivirus.png')
+			.setColor(client.embedColor)
+        message.channel.send(embed);
     },
 	name: 'virus',
 	alias: ['antivirus'],
