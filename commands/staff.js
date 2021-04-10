ff5e41d3fc1dfd159475facb7f34ded09b88c7b0
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-04-10 20:25:11 +0300

    Update staff.js

--- commands/staff.js
+++ commands/staff.js
@@ -1,19 +1,23 @@
 module.exports = {
     run: async (client, message, args) => {
+		console.log('staff command used');
 		const staff = new Map(Object.entries({
 			administrator: message.guild.roles.cache.get(client.config.mainServer.roles.administrator),
 			moderator: message.guild.roles.cache.get(client.config.mainServer.roles.moderator),
 			trialmoderator: message.guild.roles.cache.get(client.config.mainServer.roles.trialmoderator),
 			helper: message.guild.roles.cache.get(client.config.mainServer.roles.helper),
 		}));
-		const guildMembers = (await message.guild.members.fetch()).filter(x => !x.roles.cache.has(client.config.mainServer.roles.developer));
+		const guildMembers = await message.guild.members.fetch()
+		const nonDevs = guildMembers.filter(x => !x.roles.cache.has(client.config.mainServer.roles.developer));
 		let desc = '';
+		console.log('guildmembers size', guildMembers.size, 'non devs size', nonDevs.size);
 		staff.forEach((role, key) => {
-			const members = guildMembers.filter(x => x.roles.cache.has(role.id));
+			const members = nonDevs.filter(x => x.roles.cache.has(role.id));
 			if (members.size > 0) desc += '**' + role.toString() + '**\n' + members.map(x => x.toString()).join('\n') + '\n\n';
 			if (key === 'trialmoderator') desc += 'If you want to report someone or need any other moderation help, feel free to message anyone of these people.\n\n';
 			if (key === 'helper') desc += `If you have a question with the game, you are open to ping or message a ${role.toString()} to receive help.\n\n`;
 		});
+		console.log('desc', desc);
 		const embed = new client.embed()
 			.setTitle('__Staff Members__')
 			.setDescription(desc)
