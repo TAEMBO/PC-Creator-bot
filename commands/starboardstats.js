87a4604669dc2c1b9a023945fa41ea54a545c6f3
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-04-15 22:23:49 +0300

    Update starboardstats.js

--- commands/starboardstats.js
+++ commands/starboardstats.js
@@ -1,10 +1,10 @@
 module.exports = {
-	run: (client, message, args) => {
+	run: async (client, message, args) => {
 		const embed = new client.embed()
 			.setTitle('__Starboard Statistics__')
-			.setDescription(`Statistics from <#${client.config.mainServer.channels.starboard}>`)
 			.setColor(client.embedColor)
-		const bestMessages = Object.entries(client.starboard._content).filter(x => x[1].c >= 3).sort((a, b) => b[1].c - a[1].c).slice(0, 5).map(async x => {
+		const bestMessages = await Object.entries(client.starboard._content).filter(x => x[1].e).sort((a, b) => b[1].c - a[1].c).slice(0, 5).map(async x => {
+			console.log(x);
 			const starboardMessage = await client.channels.resolve(client.config.mainServer.channels.starboard).messages.resolve(x[1].e).fetch();
 			const description = starboardMessage.embeds[0].description || '';
 			`**${x.c}** :star: [Jump to Starboard](${starboardMessage.url}) ${description ? '|' : ''} ${description.length > 20 ? description.slice(18).trim() + '...' : description}`
@@ -12,9 +12,10 @@ module.exports = {
 		embed.addField('Most Starred Messages', bestMessages.join('\n'));
 
 		const starboardValues = Object.values(client.starboard._content);
+		embed.setDescription(`Statistics from <#${client.config.mainServer.channels.starboard}>\nA total of **${starboardValues.reduce((a, b) => a.c + b.c, 0)}** :star: reactions have been added.`);
 		const allUsers = Array.from(new Set(starboardValues.map(x => x.a)));
-		const bestUsers = allUsers.map(x => [x, starboardValues.filter(y => y.a === x).reduce((a, b) => a.c + b.c)]).sort((a, b) => b[1] - a[1]).slice(0, 5).map(x => `<@${x[0]}> | **${x[1]}** :star:`);
-		const bestUsers = allUsers.map(x => `<@${x}> | **${starboardValues.filter(y => y.a === x).reduce((a, b) => a.c + b.c)}** :star:`);
+		console.log('allusers', allUsers);
+		const bestUsers = allUsers.map(x => [x, starboardValues.filter(y => y.a === x).reduce((a, b) => a.c + b.c, 0)]).sort((a, b) => b[1] - a[1]).slice(0, 5).map(x => `<@${x[0]}> | **${x[1]}** :star:`);
 		embed.addField('Most Starred Users', bestUsers.join('\n'));
 		messages.channel.send(embed);
 	},
