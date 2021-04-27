69e863427bd7e518c0bfcb36fb5d2d31362e3622
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-04-27 19:23:24 +0300

    Update starboardstats.js

--- commands/starboardstats.js
+++ commands/starboardstats.js
@@ -9,22 +9,22 @@ module.exports = {
 			console.log(x);
 			const starboardMessage = await client.channels.resolve(client.config.mainServer.channels.starboard).messages.resolve(x[1].e)?.fetch();
 			if (!starboardMessage) return undefined;
-			const description = starboardMessage.embeds[0].description || '';
-			console.log(`message ${x[0]} has ${x.c} stars and embed description ${description}`);
-			return `**${x.c}** :star: [Jump to Starboard](${starboardMessage.url}) ${description ? '|' : ''} ${description.length > 20 ? description.slice(18).trim() + '...' : description}`;
+			console.log(`message ${x[0]} has ${x.c} stars and url ${starboardMessage.url}`);
+			return `**${x.c}** :star: By <@${starboardMessage.author.tag}>: [Jump to Starboard](${starboardMessage.url})`;
 		});
 		console.log(promises);
 		const bestMessages = await Promise.all(promises);
 		embed.addField('Most Starred Messages', bestMessages.join('\n') + '\n(prevent empty field value)');
 
 		const starboardValues = Object.values(client.starboard._content).filter(x => x.c >= client.starLimit);
+		console.log('starboardvalues lenght', starboardValues.length);
 		embed.setDescription(`Statistics from <#${client.config.mainServer.channels.starboard}>\nA total of **${starboardValues.reduce((a, b) => a.c + b.c, 0)}** :star: reactions have been added.`);
 		const allUsers = Array.from(new Set(starboardValues.map(x => x.a)));
 		const bestUsers = allUsers.map(x => [x, (() => {
 			const filtered = starboardValues.filter(y => y.a === x && y.c).map(x => x.c);
 			return filtered.reduce((a, b) => a + b, 0);
 		})()]);
-		const bestUsersText = bestUsers.filter(x => typeof x[1] === 'number' && !isNaN(x[1])).sort((a, b) => b[1] - a[1]).slice(0, 5).map(x => `<@${x[0]}> | **${x[1]}** :star:`);
+		const bestUsersText = bestUsers.filter(x => typeof x[1] === 'number' && !isNaN(x[1])).sort((a, b) => b[1] - a[1]).slice(0, 5).map(x => `**${x[1]}** :star: <@${x[0]}>`);
 		embed.addField('Most Starred Users', bestUsersText.join('\n') + '\n(prevent empty field value)');
 		message.channel.send(embed);
 	},
