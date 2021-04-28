5074e20590afe1332c9eaaa6d61af693784056ba
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-04-28 18:13:33 +0300

    make record look better

--- commands/record.js
+++ commands/record.js
@@ -1,10 +1,18 @@
 module.exports = {
     run: async (client, message, args) => {
-        await message.channel.send({embed: {
-			"title": "__World Record PC__",
-			"description": "This is the current World Record PC achieved by <@537292814011072522> [**Mihasa#5788**], overclocks are needed to get the highest score.\n**CPU**: 150 Base 43 Boost\n**RAM**: 4160\n**GPU**: 4340 and 24700\n• ZALMVN ZMX55 Thermal Paste is required and needs to cover 100% of the CPU.\n• Max overclocking skill is required.\n• Max spec custom CPU is required.\n\nScore achieved: __127,964__",
-			"color": 3971825}});
-		message.channel.send('https://cdn.discordapp.com/attachments/696448442989543445/836309534913069126/Untitled-1.png')
+		const achiever = {
+			id: '537292814011072522',
+			tag: 'Mihasa#5788'
+		}
+		const embed = new client.embed()
+			.setTitle('__World Record PC__')
+			.setDescription('This is the current World Record PC.')
+			.addField(':small_blue_diamond: Achieved By', `<@${achiever.id}> ${achiever.tag} (${achiever.id})`)
+			.addField(':small_blue_diamond: Details', 'overclocks are needed to get the highest score.\n**CPU**: 150 Base 43 Boost\n**RAM**: 4160\n**GPU**: 4340 and 24700\n• ZALMVN ZMX55 Thermal Paste is required and needs to cover 100% of the CPU.\n• Max overclocking skill is required.\n• Max spec custom CPU is required.')
+			.addField(':small_blue_diamond: Score Achieved', '\`127,964\`')
+			.setImage('https://cdn.discordapp.com/attachments/696448442989543445/836309534913069126/Untitled-1.png')
+			.setColor(client.embedColor)
+		message.channel.send(embed);
     },
 	name: 'record',
 	description: 'Shows the current World Record PC in the game',
