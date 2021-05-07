e0ed5a1f9c74bfe76970806b81bd902256fca20d
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2021-05-06 21:14:33 -0700

    sync with repo part 2

--- functions/hasModPerms.js
+++ functions/hasModPerms.js
@@ -1,3 +1,3 @@
 module.exports = (client, guildMember) => {
-	return client.config.mainServer.roles.staffRoles.map(x => client.config.mainServer.roles[x]).some(x => guildMember.roles.cache.has(x));
+	return client.config.mainServer.staffRoles.map(x => client.config.mainServer.roles[x]).some(x => guildMember.roles.cache.has(x));
 };
