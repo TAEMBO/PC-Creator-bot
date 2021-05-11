3b413c0dc7dc8893168d5dd8f6e82e1ba51a142f
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-05-11 18:47:00 +0300

    cooldown things

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,6 +1,9 @@
 module.exports = {
 	run: async (client, message, args) => {
-		if (message.channel.id !== client.config.mainServer.channels.suggestions) return message.channel.send(`This command only works in <#${client.config.mainServer.channels.suggestions}>`);
+		if (message.channel.id !== client.config.mainServer.channels.suggestions) {
+			client.cooldowns.get(message.author.id).set('suggest', 0);
+			return message.channel.send(`This command only works in <#${client.config.mainServer.channels.suggestions}>`);
+		}
 		await message.delete();
 		if (!args[1]) return message.reply('You need to suggest something.').then(x => setTimeout(() => x.delete(), 6000));
 		if (args[1].length > 2048) return message.reply('Your suggestion must be less than or equal to 2048 characters in length.').then(x => setTimeout(() => x.delete(), 6000));
@@ -18,5 +21,6 @@ module.exports = {
 	description: 'Create a suggestion. Only works in <#572541644755435520>',
 	category: 'PC Creator',
 	alias: ['suggestion'],
-	usage: ['suggestion']
+	usage: ['suggestion'],
+	cooldown: 10800
 };
