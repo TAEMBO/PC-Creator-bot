a6eca18728cfcb67c0c6cc34d6dbb10fe3846ef9
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-05-19 18:13:28 +0300

    new formatTime features

--- functions/formatTime.js
+++ functions/formatTime.js
@@ -1,4 +1,4 @@
-module.exports = (integer, accuracy = 1) => {
+module.exports = (integer, accuracy = 1, options = {}) => {
 	const timeNames = [
 		{ 
 			name: 'year',
@@ -27,17 +27,26 @@ module.exports = (integer, accuracy = 1) => {
 	];
 	let achievedAccuracy = 0;
 	let text = '';
+	const { longNames, commas } = options;
 	for (timeName of timeNames) {
 		if (achievedAccuracy < accuracy) {
 			const fullTimelengths = Math.floor(integer / timeName.length);
 			if (fullTimelengths === 0) continue;
 			achievedAccuracy++;
-			text += fullTimelengths + timeName.name.slice(0, timeName.name === 'month' ? 2 : 1) + ' ';
+			text += fullTimelengths + (longNames ? (' ' + timeName.name + (fullTimelengths === 1 ? '' : 's')) : timeName.name.slice(0, timeName.name === 'month' ? 2 : 1)) + (commas ? ', ' : ' ');
 			integer -= fullTimelengths * timeName.length;
 		} else {
 			break;
 		}
 	}
-	if (text.length === 0) text = '0s';
+	if (text.length === 0) text = integer + (longNames ? ' milliseconds' : 'ms') + (commas ? ', ' : '');
+	if (commas) {
+		text = text.slice(0, -2);
+		if (longNames) {
+			text = text.split('');
+			text[text.lastIndexOf(',')] = ' and';
+			text = text.join('');
+		}
+	}
 	return text.trim();
 };
