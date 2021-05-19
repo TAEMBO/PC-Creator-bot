3660b847c389d1441474a7ef29f7d084ecb7e05c
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-05-19 18:13:16 +0300

    fix error when searching for user by username

--- functions/getMember.js
+++ functions/getMember.js
@@ -1,8 +1,8 @@
 module.exports = (guild, query) => new Promise(async (res, rej) => {
 	let member;
 	if (query.match(/[a-z]|[A-Z]/g)?.length > 0) {
-		member = await guild.members.fetch({ query: query, limit: 1, time: 10000 }).catch(() => rej());
-		if (!member.first()) rej();
+		member = (await guild.members.fetch({ query: query, limit: 1, time: 10000 }).catch(() => rej()))?.first();
+		if (!member) rej();
 	} else {
 		member = await guild.members.fetch(query).catch(() => rej());
 		if (!member) rej();
