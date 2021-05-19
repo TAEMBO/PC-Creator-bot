7cf8e42c25f6808f1b3a91ef2dfeae40ddd623a6
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-05-19 18:49:08 +0300

    dont give lvl1 role to people who are not you

--- commands/applyforbetterchat.js
+++ commands/applyforbetterchat.js
@@ -8,8 +8,10 @@ module.exports = {
 		if (age && messages) {
 			if (member.roles.cache.has(role.id)) return message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'} already have the **${role.name}** role.`);
 			await message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'}\'re eligible for access to the **${role.name}** role.`);
-			await member.roles.add(role.id);
-			message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'}\'ve received the **${role.name}** role. You can now access <#${client.config.mainServer.channels.betterGeneral}>`);
+			if (message.author.id === member.user.id) {
+				await member.roles.add(role.id);
+				message.channel.send(`You\'ve received the **${role.name}** role. You can now access <#${client.config.mainServer.channels.betterGeneral}>`);
+			}
 		} else {
 			message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'}\'re not eligible for access to the **${role.name}** role. Progress:\n${messages ? ':white_check_mark:' : ':x:'} ${client.userLevels.getUser(member.user.id)}/${client.userLevels._requirements.messages} messages\n${age ? ':white_check_mark:' : ':x:'} ${Math.floor((Date.now() - member.joinedTimestamp) / 1000 / 60 / 60 / 24)}d/${Math.floor(client.userLevels._requirements.age / 1000 / 60 / 60 / 24)}d time on server.`);
 		}
