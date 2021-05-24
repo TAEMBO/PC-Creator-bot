ab82e36f815aafb287350797e41d96e65f00bbd6
TÆMBØ <kuneshr123@gmail.com>
2021-05-24 15:42:10 -0700

    updating eggs.js, cost.js, bitcoinminer.js

--- commands/cost.js
+++ commands/cost.js
@@ -2,5 +2,6 @@ module.exports = {
 	run: (client, message, args) => {
 		message.channel.send('https://canary.discord.com/channels/571031703661969430/571031705109135361/845037393551687690');
 	},
-	name: 'cost'
+	name: 'cost',
+	description: 'Is PC Creator 2 going to be free?'
 };
--- commands/eggs.js
+++ commands/eggs.js
@@ -2,5 +2,7 @@ module.exports = {
 	run: (client, message, args) => {
 		message.channel.send('https://cdn.discordapp.com/attachments/787590275110273035/845496741167104010/unknown.png');
 	},
-	name: 'eggs'
+	name: 'eggs',
+	category: 'PC Creator',
+	description: 'Shows every egg in the Easter event'
 };
