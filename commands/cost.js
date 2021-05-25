6171bd248a05c01178f496891236df5e2e70f45c
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-05-25 17:52:29 +0300

    Update cost.js

--- commands/cost.js
+++ commands/cost.js
@@ -3,5 +3,6 @@ module.exports = {
 		message.channel.send('https://canary.discord.com/channels/571031703661969430/571031705109135361/845037393551687690');
 	},
 	name: 'cost',
-	description: 'Is PC Creator 2 going to be free?'
+	description: 'Is PC Creator 2 going to be free?',
+	category: 'PC Creator'
 };
