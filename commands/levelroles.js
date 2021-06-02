c574a55d06d9db6503b35e496154f47a329c1b07
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-06-02 14:54:39 +0300

    cleanup

--- commands/levelroles.js
+++ commands/levelroles.js
@@ -8,19 +8,15 @@ module.exports = {
 
 		// information about users progress on level roles
 		const eligiblity = await client.userLevels.getEligible(member);
-		console.log(eligiblity);
 
 		// index of next role
 		const nextRoleKey = eligiblity.roles.map(x => x.role.has).indexOf(false);
-		console.log(nextRoleKey);
 
 		// eligibility information about the next level role
 		const nextRoleReq = eligiblity.roles[nextRoleKey];
-		console.log(nextRoleReq);
 
 		// next <Role> in level roles that user is going to get 
 		const nextRole = nextRoleReq ? message.guild.roles.cache.get(nextRoleReq.role.id) : undefined;
-		console.log(nextRole?.name);
 
 		// level roles that user has, formatted to "1, 2 and 3"
 		let achievedRoles = eligiblity.roles.filter(x => x.role.has).map(x => '**' + message.guild.roles.cache.get(x.role.id).name + '**');
@@ -29,8 +25,6 @@ module.exports = {
 			else if (achievedRoles.length === 1 || i === achievedRoles.length - 1) return x;
 			else return x + ', ';
 		}).join('');
-		console.log(achievedRoles);
-
 		
 		function progressText(showRequirements = true) { // shows progress, usually to next milestone
 			let text = '';
@@ -51,7 +45,6 @@ module.exports = {
 			if (message.author.id === member.user.id) return you || true;
 			else return they || false;
 		};
-		console.log(pronounBool('you', 'they'));
 		
 		const messageContents = [];
 
@@ -81,28 +74,6 @@ module.exports = {
 		}
 
 		message.channel.send(messageContents.join('\n')); // compile message and send
-
-
-
-
-		
-		
-		
-		/*const member = args[1] ? message.mentions.members?.first() || (await client.getMember(message.guild, args[1]).catch(() => { })) : message.member;
-		if (!member) return message.channel.send('You failed to mention a user from this server.');
-		const age = member.joinedTimestamp < Date.now() - client.userLevels._requirements.age;
-		const messages = client.userLevels.getEligible(member.user.id);
-		const role = message.guild.roles.cache.get(client.config.mainServer.roles.levelOne);
-		if (age && messages) {
-			if (member.roles.cache.has(role.id)) return message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'} already have the **${role.name}** role, but since you asked, heres ${member.user.id === message.author.id ? 'your' : 'their'} progress:\n${messages ? ':white_check_mark:' : ':x:'} ${client.userLevels.getUser(member.user.id)}/${client.userLevels._requirements.messages} messages\n${age ? ':white_check_mark:' : ':x:'} ${Math.floor((Date.now() - member.joinedTimestamp) / 1000 / 60 / 60 / 24)}d/${Math.floor(client.userLevels._requirements.age / 1000 / 60 / 60 / 24)}d time on server.`);
-			await message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'}\'re eligible for access to the **${role.name}** role.${member.user.id === message.author.id ? '' : ` Their progress:\n${messages ? ':white_check_mark: ' : ': x: '} ${client.userLevels.getUser(member.user.id)}/${client.userLevels._requirements.messages} messages\n${age ? ':white_check_mark: ' : ': x:'} ${Math.floor((Date.now() - member.joinedTimestamp) / 1000 / 60 / 60 / 24)}d/${Math.floor(client.userLevels._requirements.age / 1000 / 60 / 60 / 24)}d time on server.`}`);
-			if (message.author.id === member.user.id) {
-				await member.roles.add(role.id);
-				message.channel.send(`You\'ve received the **${role.name}** role. You can now access <#${client.config.mainServer.channels.betterGeneral}>`);
-			}
-		} else {
-			message.channel.send(`${member.user.id === message.author.id ? 'You' : 'They'}\'re not eligible for access to the **${role.name}** role. Progress:\n${messages ? ':white_check_mark:' : ':x:'} ${client.userLevels.getUser(member.user.id)}/${client.userLevels._requirements.messages} messages\n${age ? ':white_check_mark:' : ':x:'} ${Math.floor((Date.now() - member.joinedTimestamp) / 1000 / 60 / 60 / 24)}d/${Math.floor(client.userLevels._requirements.age / 1000 / 60 / 60 / 24)}d time on server.`);
-		}*/
 	},
 	name: 'levelroles',
 	description: 'Check your eligibility for level roles.',
