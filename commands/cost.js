0fc8d79c90d727ab07fd2216394ab56bf89d9f65
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-06-05 14:02:16 +0300

    pcc2 paid, update ,cost

--- commands/cost.js
+++ commands/cost.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: (client, message, args) => {
-		message.channel.send('https://canary.discord.com/channels/571031703661969430/571031705109135361/845037393551687690');
+		message.channel.send('https://canary.discord.com/channels/571031703661969430/571031705109135361/845037393551687690\nEDIT: Boss has confirmed that PCC2 will not be free. https://discord.com/channels/571031703661969430/571031705109135361/850690495343755274');
 	},
 	name: 'cost',
 	description: 'Is PC Creator 2 going to be free?',
