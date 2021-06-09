bc7292be37abe8c9a22e12273485649943db6eaf
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-06-09 18:43:17 +0300

    make ping more confusing

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
 		message.channel.send(`WebSocket Latency: **${client.ws.ping}ms**`);
 	},
 	name: 'ping',
-	description: 'Average latency of bot client\'s shards\' last heartbeat from sending to ack in milliseconds. No it\'s not _your_ ping.',
+	description: 'Average latency of bot client\'s shards\' last websocket heartbeat from sending to ack in milliseconds. No it\'s not _your_ ping.',
 	shortDescription: 'Average Websocket latency'
 };
