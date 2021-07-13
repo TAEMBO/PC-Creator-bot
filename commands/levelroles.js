9a5e0f66ecdd8fa5373a71b2afb94b6930de3129
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-07-13 03:08:55 +0300

    fix month being only 4 days long, prohibit usage of command in ts

--- commands/levelroles.js
+++ commands/levelroles.js
@@ -1,5 +1,6 @@
 module.exports = {
 	run: async (client, message, args) => {
+		if (message.guild.id !== client.config.mainServer.id) return message.channel.send('This command doesn\'t work in this server.');
 		// if args[1] is "stats", show stats
 		if (args[1] === 'stats') {
 			// days since mar 22, 2021 when userlevels was created
@@ -20,7 +21,7 @@ module.exports = {
 			// dailymsgs.json
 			const dailyMsgs = require('../dailyMsgs.json');
 			// days to get average from
-			const dataLength = 4
+			const dataLength = 30;
 			// last 30d, sorted by date ascending
 			const lastMonth = Object.entries(dailyMsgs).sort((a, b) => {
 				const aDate = a[0].split('-').map(x => parseInt(x));
@@ -51,7 +52,7 @@ module.exports = {
 
 			const embed = new client.embed()
 				.setTitle('Level Roles: Stats')
-				.setDescription(`Level Roles was created ${timeActive} days ago.\nSince then, a total of ${messageCountsTotal.toLocaleString('en-US')} messages have been sent in this server by ${userCount.toLocaleString('en-US')} users.\nIn the last 30 days, on average, ${averageMsgsPerDay.toLocaleString('en-US', { maximumFractionDigits: 2 })} messages have been sent every day.\nAn average user has sent ${average.toFixed(2)} messages.\n${((messageCounts.filter(x => x >= average).length / userCount) * 100).toFixed(2)}% of users have sent more than or as many messages as an average user.\nThe median user has sent ${median} messages.\nThe top 1% of users have sent ${((messageCounts.sort((a, b) => b - a).slice(0, Math.round(userCount / 100)).reduce((a, b) => a + b, 0) / messageCountsTotal) * 100).toLocaleString('en-US', { maximumFractionDigits: 2 })}% of messages while Level Roles has existed.\nThe next message milestone ${milestone.next ? `is ${milestone.next.toLocaleString('en-US')} messages and the progress from the previous milestone (${milestone.previous.toLocaleString('en-US')}) to the next is ${(milestone.progress * 100).toFixed(2)}%.\nAt the current rate, reaching the next milestone would take ${client.formatTime(millisecondsToMilestone, 2, { commas: true, longNames: true })}.` : `doesn\'t exist.`}`)
+				.setDescription(`Level Roles was created ${timeActive} days ago.\nSince then, a total of ${messageCountsTotal.toLocaleString('en-US')} messages have been sent in this server by ${userCount.toLocaleString('en-US')} users.\nIn the last ${dataLength} days, on average, ${averageMsgsPerDay.toLocaleString('en-US', { maximumFractionDigits: 2 })} messages have been sent every day.\nAn average user has sent ${average.toFixed(2)} messages.\n${((messageCounts.filter(x => x >= average).length / userCount) * 100).toFixed(2)}% of users have sent more than or as many messages as an average user.\nThe median user has sent ${median} messages.\nThe top 1% of users have sent ${((messageCounts.sort((a, b) => b - a).slice(0, Math.round(userCount / 100)).reduce((a, b) => a + b, 0) / messageCountsTotal) * 100).toLocaleString('en-US', { maximumFractionDigits: 2 })}% of messages while Level Roles has existed.\nThe next message milestone ${milestone.next ? `is ${milestone.next.toLocaleString('en-US')} messages and the progress from the previous milestone (${milestone.previous.toLocaleString('en-US')}) to the next is ${(milestone.progress * 100).toFixed(2)}%.\nAt the current rate, reaching the next milestone would take ${client.formatTime(millisecondsToMilestone, 2, { commas: true, longNames: true })}.` : `doesn\'t exist.`}`)
 				.addField('Top Users by Messages Sent', Object.entries(client.userLevels._content).sort((a, b) => b[1] - a[1]).slice(0, 5).map((x, i) => `\`${i + 1}.\` <@${x[0]}>: ${x[1].toLocaleString('en-US')}`).join('\n'))
 				.setColor(client.embedColor)
 			message.channel.send(embed);
