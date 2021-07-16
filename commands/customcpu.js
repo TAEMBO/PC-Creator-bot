260d5353cc2b6cab09e96ea92830eaac8f657351
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-07-16 23:34:47 +0300

    add footer to customcpu

--- commands/customcpu.js
+++ commands/customcpu.js
@@ -3,6 +3,7 @@ module.exports = {
 		const embed = new client.embed()
 			.setTitle('This is how custom CPUs were made.')
 			.setImage("https://media.discordapp.net/attachments/696448442989543445/865040800646561834/Screenshot_2021-02-12-17-34-34-411_com.png")
+			.addFooter('You cannot make custom CPUs anymore. The feature to manufacture them was removed in an update.')
 			.setColor(client.embedColor)
 		message.channel.send(embed);
 	},
