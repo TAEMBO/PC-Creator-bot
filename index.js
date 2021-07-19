02cad9634278322013a99b3f5f80a89b1b4ba229
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-07-19 17:23:07 +0300

    repeated messages only in mainserver

--- index.js
+++ index.js
@@ -667,7 +667,7 @@ client.on("message", async (message) => {
 		}
 	} else {
 		// repeated messages
-		if (message.content.length > 10) {
+		if (message.content.length > 10 && message.guild.id === client.config.mainServer.id) {
 			const thisContent = message.content.slice(0, 32);
 			if (client.repeatedMessages[message.author.id]) {
 				// add this message to the list
