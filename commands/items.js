3c68d830a9e32cb1551658a82ffd969f61cd025f
TÆMBØ <kuneshr123@gmail.com>
2021-07-20 16:43:50 -0700

    delete un-needed commands, make cpu and gpu databases public

--- commands/items.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	run: (client, message, args) => {
-		message.channel.send('https://cdn.discordapp.com/attachments/778848112588095559/818526664614477834/everything_2020_11_08_17_40_41_UTC.png');
-	},
-	name: 'items',
-	description: 'Provides a picture of each type of item',
-	category: 'PC Creator'
-};
