41172a4fcd6d77243770e8a4f9d86222320754fb
TÆMBØ <kuneshr123@gmail.com>
2021-08-01 00:11:05 -0700

    fix invalid image

--- commands/customcpu.js
+++ commands/customcpu.js
@@ -2,7 +2,7 @@ module.exports = {
 	run: (client, message, args) => {
 		const embed = new client.embed()
 			.setTitle('This is how custom CPUs were made.')
-			.setImage("https://media.discordapp.net/attachments/696448442989543445/865040800646561834/Screenshot_2021-02-12-17-34-34-411_com.png")
+			.setImage("https://media.discordapp.net/attachments/571031705109135361/809885303119675482/Screenshot_2021-02-12-17-34-34-411_com.ultraandre.pccreator.jpg")
 			.setFooter('You cannot make custom CPUs anymore. The feature to manufacture them was removed in an update.')
 			.setColor(client.embedColor)
 		message.channel.send(embed);
