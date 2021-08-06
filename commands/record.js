fd2c892c9fc2fd8698be1620b5bb6b20e154700c
TÆMBØ <kuneshr123@gmail.com>
2021-08-06 01:15:38 -0700

    Update record.js

--- commands/record.js
+++ commands/record.js
@@ -9,8 +9,8 @@ module.exports = {
 			.setDescription('This is the current World Record PC.')
 			.addField(':small_blue_diamond: Achieved By', `<@${achiever.id}> ${achiever.tag} (${achiever.id})`)
 			.addField(':small_blue_diamond: Details', 'overclocks are needed to get the highest score.\n**CPU**: 150 Base 43 Boost\n**RAM**: 13000\n**GPU**: 7849 and 40300\n• ZALMVN ZMX55 Thermal Paste is required and needs to cover 100% of the CPU.\n• Max overclocking skill is required.\n• Max spec custom CPU is required.')
-			.addField(':small_blue_diamond: Score Achieved', '\`152,172\`')
-			.setImage('https://cdn.discordapp.com/attachments/838857610358292532/872543295765622864/image0.png')
+			.addField(':small_blue_diamond: Score Achieved', '\`155,390\`')
+			.setImage('https://media.discordapp.net/attachments/838857610358292532/873115363355799552/image1.png')
 			.setColor(client.embedColor)
 		message.channel.send(embed);
     },
