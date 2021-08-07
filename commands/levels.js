d9c33b75a53dcb68c94b7ae9b589a2a0a33bd435
TÆMBØ <kuneshr123@gmail.com>
2021-08-07 13:26:04 -0700

    Update levels.js

--- commands/levels.js
+++ commands/levels.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: (client, message, args) => {
-		message.channel.send("https://cdn.discordapp.com/attachments/802657645039321131/841724579327115284/unknown.png");
+		message.channel.send("https://cdn.discordapp.com/attachments/571031705109135361/873663041458278450/unknown.png");
 	},
 	name: 'levels',
 	description: 'Shows each level in PC Creator and how much XP it takes to get for each level',
