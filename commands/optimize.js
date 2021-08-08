6ab3866d2190ff9b8d08a37f73e48d55f198ff0b
TÆMBØ <kuneshr123@gmail.com>
2021-08-07 22:23:50 -0700

    FPS not OS

--- commands/optimize.js
+++ commands/optimize.js
@@ -3,7 +3,7 @@ module.exports = {
         message.channel.send("The goal of this requirement is to make the PC get the specified FPS in the specified game, meaning you must test hardware and play the game on the PC till you find something that meets or beats the requirement. Parts that affect FPS are CPUs, RAM, and GPUs.");
     },
 	name: 'optimize',
-	description: 'Explains how to complete an optimize OS order',
+	description: 'Explains how to complete an optimize FPS order',
 	category: 'PC Creator',
     alias: ['opt'],
 	autores: ['how/what', 'optimize']
