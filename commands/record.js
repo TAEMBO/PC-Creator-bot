4de1a5f0cedba53a2b133e5d04f27dec93bfe1b7
TÆMBØ <kuneshr123@gmail.com>
2021-08-09 17:16:21 -0700

    gpu oc 7840 not 7849

--- commands/record.js
+++ commands/record.js
@@ -8,7 +8,7 @@ module.exports = {
 			.setTitle('__World Record PC__')
 			.setDescription('This is the current World Record PC.')
 			.addField(':small_blue_diamond: Achieved By', `<@${achiever.id}> ${achiever.tag} (${achiever.id})`)
-			.addField(':small_blue_diamond: Details', 'overclocks are needed to get the highest score.\n**CPU**: 153 Base\n**RAM**: 13000\n**GPU**: 7849 and 40300\n• ZALMVN ZMX55 Thermal Paste is required and needs to cover 100% of the CPU.\n• Max overclocking skill is required.')
+			.addField(':small_blue_diamond: Details', 'overclocks are needed to get the highest score.\n**CPU**: 153 Base\n**RAM**: 13000\n**GPU**: 7840 and 40300\n• ZALMVN ZMX55 Thermal Paste is required and needs to cover 100% of the CPU.\n• Max overclocking skill is required.')
 			.addField(':small_blue_diamond: Score Achieved', '\`183,446\`')
 			.setImage('https://media.discordapp.net/attachments/838857610358292532/874396109907329074/image2.png')
 			.setColor(client.embedColor)
