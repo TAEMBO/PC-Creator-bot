e298a8e7c3848834c7b6a4f1a0d0bc92779fe407
TÆMBØ <kuneshr123@gmail.com>
2021-09-09 10:49:55 -0700

    revamp btcpc, and meme idle

--- commands/bitcoinminer.js
+++ commands/bitcoinminer.js
@@ -1,58 +1,6 @@
 module.exports = {
     run: async (client, message, args) => {
-		const embed = new client.embed()
-			.setTitle("Best Bitcoin build")
-			.setDescription("All items in this list are buyable with in-game money.\nCost: $2980")
-			.setColor(client.embedColor)
-			.addFields([
-				{
-					"name": "Case",
-					"value": "N/A",
-					"inline": true
-				},
-				{
-					"name": "Motherboard",
-					"value": "ASOS MAXIMUS VIII",
-					"inline": true
-				},
-				{
-					"name": "CPU",
-					"value": "INTOL G3900",
-					"inline": true
-				},
-				{
-					"name": "RAM",
-					"value": "L15S 4GB",
-					"inline": true
-				},
-				{
-					"name": "GPUS",
-					"value": "3x GEFORCE RTX 2060",
-					"inline": true
-				},
-				{
-					"name": "Storage",
-					"value": "DW GREEN DWS120",
-					"inline": true
-				},
-				{
-					"name": "PSU",
-					"value": "MWE RED",
-					"inline": true
-				},
-				{
-					"name": "Cooler",
-					"value": "DEEPCOL 15 PWM",
-					"inline": true
-				},
-				{
-					"name": "\u200b",
-					"value": "\u200b",
-					"inline": true
-				}
-			])
-			.setImage('https://media.discordapp.net/attachments/571031705109135361/873973732874551416/Bitcoin_Pc.jpg')
-        message.channel.send(embed);
+        message.channel.send('https://cdn.discordapp.com/attachments/838857610358292532/885238002819952770/unknown.png');
     },
 	name: 'bitcoinminer',
 	description: 'Shows the current best bitcoin mining setup for only in-game money.',
