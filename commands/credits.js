04cf9fa1eca8a1a32dfc958a3f3c3977f8780027
TÆMBØ <kuneshr123@gmail.com>
2021-09-11 11:43:45 -0700

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -2,10 +2,12 @@ module.exports = {
 	run: (client, message, args) => {
 		const embed = new client.embed()
 			.setTitle('Credits')
-            .addField('Bot Owner', '<@615761944154210305>')
+            .addField('Bot Owner/Developer', '<@615761944154210305>')
             .addField('Bot Developer', '<@384002606621655040>')
             .addField('CPU data', '<@615761944154210305> <@731186471461060641> <@506033590656696332> <@491620401810767881>')
-            .addField('GPU data', '<@615761944154210305> <@731186471461060641> <@469571345810718721> <@491620401810767881> <@506033590656696332>')
+            .addField('GPU data', '<@615761944154210305> <@731186471461060641> <@491620401810767881> <@506033590656696332>')
+			.addField(',scores sheets', '<@695229647021015040>')
+			.addField('Chipset data', '<@615761944154210305>')
 			.setColor(client.embedColor)
 		message.channel.send(embed);
 	},
