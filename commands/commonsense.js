81b232c992d91bda9e94d78d74ad03c81292c09d
TÆMBØ <kuneshr123@gmail.com>
2021-11-06 12:36:35 -0700

    give them bitches knowledge

---
+++ commands/commonsense.js
@@ -0,0 +1,11 @@
+module.exports = {
+	run: (client, message, args) => {
+		const embed = new client.embed()
+			.setTitle('Using common sense.')
+			.setImage("https://childdevelopment.com.au/wp-content/uploads/bfi_thumb/shape-sorter-2xjqawyb3s7qeuj8vk2jml3ym5m6yrbslxqx94shln1zpdhaa.jpg")
+			.setColor(client.embedColor)
+		message.channel.send(embed);
+	},
+	name: 'cs',
+    hidden: true
+};
