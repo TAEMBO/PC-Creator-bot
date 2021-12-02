2a39a61f8676c52322cb6cbf4d5de4ceaa33142a
TÆMBØ <kuneshr123@gmail.com>
2021-12-01 21:41:54 -0800

    new pc version of pcc?????

--- commands/emulator.js
+++ commands/emulator.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: (client, message, args) => {
-		message.channel.send('To play PC Creator on a PC, you need to use an emulator. The game is not available on desktop yet. Some popular emulators are LDPlayer, MEmu, NoxPlayer');
+		message.channel.send('To play PC Creator on a PC, you can download the Steam version at https://store.steampowered.com/app/1831530/PC_Creator__PC_Building_Simulator/ or you can use an Android emulator. Some popular emulators are LDPlayer, MEmu, and NoxPlayer.');
 	},
 	name: 'emulator',
 	alias: ['bluestacks', 'memu'],
