c1f78f8ce2aa3a0a8348dbab0b9c9303efa2b8cf
TÆMBØ <kuneshr123@gmail.com>
2021-12-01 19:36:08 -0800

    dead memes begone

--- autores.js
+++ autores.js
@@ -1,12 +1,6 @@
 module.exports = async (message, client) => {
-	if (message.content.toLowerCase().includes("titanus") && Math.random() < 3 / 7) {
-		message.channel.send("Ass-sus tit anus <:hahaha6:740166145167982623>");
-	}
 	if (message.content.includes("userbenchmark.com")) {
 		message.reply(":b:ingus y u use userbenchmark");
-	}
-	if (message.content.toLowerCase().includes("uwu")) {
-		message.reply("You received an honorary ban!");
 	}
 	if (message.author.id === '155149108183695360' /* dyno */ && ['was muted', 'was banned', 'has been warned', 'was softbanned'].some(x => message.embeds[0]?.description?.includes(x))) {
 		message.channel.send(':partying_face: :tada:');
