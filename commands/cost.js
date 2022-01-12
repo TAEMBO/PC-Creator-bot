34385819bee17890c312c54d16da6574132f0f47
AseCoder <51131379+AseCoder@users.noreply.github.com>
2022-01-12 20:19:14 +0200

    Update cost.js

--- commands/cost.js
+++ commands/cost.js
@@ -15,11 +15,21 @@ module.exports = {
 				time: 1623596897340,
 				url: 'https://canary.discord.com/channels/571031703661969430/571031705109135361/853652001899151411',
 				text: '<@321615117550616588> estimated the price of PCC2 between 5 and 8 US Dollars ($).'
+			},
+			{
+				time: 1641923410520,
+				url: 'https://discord.com/channels/571031703661969430/572536109754744839/930518969436217394',
+				text: '<@321615117550616588> announced that PCC2 is available for pre-order.'
+			},
+			{
+				time: 1642011294369,
+				url: 'https://discord.com/channels/571031703661969430/571031705109135361/930887581015605309',
+				text: '<@615761944154210305> found that **the game costs 7.99 US Dollars ($)**.'
 			}
 		];
 		const embed = new client.embed()
 			.setTitle('The Cost of PCC2')
-			.setDescription(`Something has been said ${events.length} times about the cost of the upcoming sequel to the PC Creator mobile game.`)
+			.setDescription(`Something has been said ${events.length} times about the cost of the sequel to the PC Creator mobile game.`)
 			.setColor(client.embedColor)
 		events.forEach(e => {
 			embed.addField(`On <t:${Math.floor(e.time / 1000)}:d>,`, e.text + `\n[Message](${e.url})`);
